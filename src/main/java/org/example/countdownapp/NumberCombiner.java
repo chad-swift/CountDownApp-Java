1286cca7ac7ee080ad1d348e3d7dd4de709fa87a
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2026-08-12 18:24:22 -0400

    reworked all algorithms to be tinier building blocsk for the puzzle, swapped to permutations instead of combinations

--- src/main/java/org/example/countdownapp/NumberCombiner.java
+++ src/main/java/org/example/countdownapp/NumberCombiner.java
@@ -11,56 +11,79 @@ public class NumberCombiner {
     static private int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6;
 
     public static void main(String[] args) {
-        IGenerator<List<Integer>> twos = Generator.combination(a, b, c, d, e, f).simple(2);
-        IGenerator<List<Integer>> threes = Generator.combination(a, b, c, d, e, f).simple(3);
 
-        Map<String, List<Integer>> twoMap = new HashMap<>();
+        /*
+        are there only ever actually four patterns here?
+        abcdef
+        ab cd ef
+        a bc d ef
+        ab c de f
+        a bc de f
+         */
 
-        for (List<Integer> pair : twos) {
-            twoMap.put("" + pair.getFirst() + pair.get(1), calculate(pair));
-        }
-
-        for (List<Integer> value : twoMap.values()) {
-            System.out.println(value);
-        }
+        allTwosPattern(a, b, c, d, e, f);
     }
 
-    public static List<Integer> calculate(List<Integer> nums) {
+    public static void allTwosPattern(int a, int b, int c, int d, int e, int f) {
+        IGenerator<List<Integer>> permutations = Generator.permutation(a, b, c, d, e, f).simple();
+            List<List<Integer>> step1 = new ArrayList<>();
+
+        for (List<Integer> list : permutations) {
+            for(List<Integer> step1Result : reduce(list)) {
+                for (List<Integer> step2Result : reduce(step1Result)) {
+                    for (List<Integer> step3Result : reduce(step2Result)) {
+                        for (List<Integer> step4Result: reduce(step3Result)) {
+                           for (int answer : calculate(step4Result.getFirst(), step4Result.get(1))) {
+                               System.out.println(answer);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static List<List<Integer>> reduce(List<Integer> list) {
+        List<Integer> listCopy = new ArrayList<>(list);
+        List<List<Integer>> output = new ArrayList<>();
+
+        int first = listCopy.getFirst();
+        int second = listCopy.get(1);
+
+        listCopy.removeFirst();
+        listCopy.remove(1);
+
+        List<Integer> calculatedPair = calculate(first, second);
+
+        for (int result: calculatedPair) {
+            List<Integer> timeline = new ArrayList<>(listCopy);
+            timeline.add(1, result);
+            output.add(timeline);
+        }
+        return output;
+    }
+
+    public static List<Integer> calculate(int a, int b) {
 
         ArrayList<Integer> list = new ArrayList<>();
 
-        int num1 = nums.getFirst();
-        int num2 = nums.get(1);
-
-        System.out.println("Checking " + num1 + " and " + num2 + "...");
-
-        int sum = num1 + num2;
-        System.out.println(" "+num1 + " + " + num2 + " = " + sum);
-        int product = num1 * num2;
-        System.out.println(" "+num1 + " x " + num2 + " = " + product);
-        int difference = Math.abs(num1 - num2);
-        System.out.println(" "+num1 + " - " + num2 + " = " + difference);
+        int sum = a + b;
+        int product = a * b;
 
         list.add(sum);
         list.add(product);
 
-        if (difference > 0) {
+        if (a > b) {
+            int difference = a - b;
             list.add(difference);
         }
 
-        if (num1 > num2) {
-            if (num1 % num2 == 0) {
-                int division = num1 / num2;
-                System.out.println(" "+num1 + " / " + num2 + " = " + division);
-                list.add(division);
-            }
-        } else {
-            if (num2 % num1 == 0) {
-                int division = num2 / num1;
-                System.out.println(" "+num2 + " / " + num1 + " = " + division);
-                list.add(division);
-            }
+        if (a > b && a % b == 0) {
+            int quotient = a / b;
+            list.add(quotient);
         }
+
         return list;
     }
 }
