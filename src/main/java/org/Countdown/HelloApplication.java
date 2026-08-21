84dc767885cd7c9f7df371d48eda2e0b8dce0d5f
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2026-08-21 16:20:58 -0400

    package name changes

--- src/main/java/org/Countdown/HelloApplication.java
+++ src/main/java/org/Countdown/HelloApplication.java
@@ -1,4 +1,4 @@
-package org.example.countdownapp;
+package org.Countdown;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/main/java/org/Countdown/HelloController.java
+++ src/main/java/org/Countdown/HelloController.java
@@ -1,4 +1,4 @@
-package org.example.countdownapp;
+package org.Countdown;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
