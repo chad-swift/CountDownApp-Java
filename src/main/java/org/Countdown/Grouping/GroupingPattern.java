279146fef44066f88783335eeb2bcfd443429301
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2026-08-30 14:00:37 -0400

    created new method for calculating and checking the target

--- src/main/java/org/Countdown/Grouping/GroupingPattern.java
+++ src/main/java/org/Countdown/Grouping/GroupingPattern.java
@@ -5,28 +5,30 @@ import org.Countdown.Number.CountdownNumber;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.Countdown.Number.CountDownNumberCalculator.calculate;
+
 public abstract class GroupingPattern implements CountDownExecutable {
     private final List<CountdownNumber> searchList = new ArrayList<>();
-    private final List<List<CountdownNumber>> results = new ArrayList<>();
+    protected final AnswerChecker checker;
 
     GroupingPattern() {
+        this.checker = new AnswerChecker();
     }
 
-    protected GroupingPattern(List<CountdownNumber> list) {
-        searchList.addAll(list);
-    }
-
-    public List<List<CountdownNumber>> getResults() {
-        List<List<CountdownNumber>> resultsCopy = new ArrayList<>();
-
-        for (List<CountdownNumber> list : results) {
-            results.add(new ArrayList<>(list));
-        }
-
-        return resultsCopy;
+    protected GroupingPattern(List<CountdownNumber> list, AnswerChecker checker) {
+        this.searchList.addAll(list);
+        this.checker = checker;
     }
 
     public List<CountdownNumber> getSearchList() {
         return new ArrayList<>(searchList);
     }
+
+    public AnswerTuple<CountdownNumber> calculateAndCheckForTarget(CountdownNumber a, CountdownNumber b) {
+        List<CountdownNumber> results = calculate(a, b);
+        boolean bool = this.checker.checkAndAddAnswerToBank(results);
+
+        return new AnswerTuple<>(results, bool);
+    }
+
 }
